a32871a5704974fe7907ad2b0c9358b5f15feef0
Tejash Agrawal <72141770+tej95@users.noreply.github.com>
2025-01-11 02:56:52 +0530

    Update App.jsx

--- vite-project-login/src/App.jsx
+++ vite-project-login/src/App.jsx
@@ -12,6 +12,7 @@ function App() {
   return (
   <Router>
     <Routes>
+      <Route path="/" element={<Signup />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
       <Route path="/home" element={<Home />} />
